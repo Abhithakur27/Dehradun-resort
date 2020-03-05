13c08648557262e33b250d1ab82e86f71617e56a
Abhishek Thakur <45519405+Abhithakur27@users.noreply.github.com>
2020-03-05 14:23:37 +0530

    components navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Pages/Home";
+import Rooms from "./Pages/Rooms";
+import SingleRoom from "./Pages/SingleRoom";
+import Error from "./Pages/Error";
+import Navbar from "./Components/Navbar";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route excat path="/Rooms" component={Rooms} />
+          <Route excat path="/Rooms/:slug" component={SingleRoom} />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
